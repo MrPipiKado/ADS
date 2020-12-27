9e199492e4433d4c27f67a21dc2032215dc4e61f
MrPipiKado <ammamm23102310@gmail.com>
2020-12-27 20:28:26 +0200

    Lab12 done

---
+++ Lab12/Lab12/mainwindow.cpp
@@ -0,0 +1,92 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <iostream>
+#include <fstream>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    std::ifstream file1("/home/user/lab12_1.txt");
+    std::string line;
+
+    if (file1.is_open()) {
+        while (getline (file1,line)){
+          text1 += QString::fromStdString(line);
+        }
+        file1.close();
+    }
+    ui->first_text->setText(text1);
+    std::ifstream file2("/home/user/lab12_2.txt");
+
+    if (file2.is_open()) {
+        while (getline (file2,line)) {
+          text2 += QString::fromStdString(line);
+        }
+        file2.close();
+    }
+    ui->second_text->setText(text2);
+}
+
+int BoyerMooreSearch(QString text, QString word, QString & log){
+    int i = 0;
+    int j, stop;
+
+    while(i < text.size() - word.size()){
+        j = word.size() - 1;
+        stop = word.size();
+        while(j >= 0 && text[i+j] == word[j]){
+            log += "Comparing " + word[j] + " to " + text[i+j] + ", match\n";
+            j--;
+        }
+        if(j == -1){
+            return i;
+        }
+        stop = j;
+        if(stop == word.size()){
+            i = i + word.size();
+        } else if (j > stop) {
+            i = i + j - stop;
+        } else {
+            i++;
+        }
+    }
+    return -1;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QStringList words = text1.split(QRegExp("\\s+"), QString::SkipEmptyParts);
+    QString log = " ";
+    word = words[words.size()/2];
+    ui->label->setText(word);
+    for(int i = 0; i<word.size(); ++i)
+    {
+        if(word[i]=='a') word[i] = 'o';
+        if(word[i]=='i') word[i] = 'e';
+    }
+    ui->label_2->setText(word);
+
+    int index = BoyerMooreSearch(text2, word, log);
+    ui->label_3->setText(QString::number(index));
+
+    QTextCharFormat fmt;
+    fmt.setBackground(Qt::yellow);
+    QTextCursor cursor(ui->second_text->document());
+    cursor.setPosition(index, QTextCursor::MoveAnchor);
+    cursor.setPosition(index + word.size(), QTextCursor::KeepAnchor);
+    cursor.setCharFormat(fmt);
+    log += "\nFound word at index: " + QString::number(index);
+
+    ui->textEdit_3->setText(log);
+}
---
+++ Lab12/build-Lab12-Desktop_Qt_5_14_1_GCC_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,102 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QTextEdit *first_text;
+    QTextEdit *second_text;
+    QTextEdit *textEdit_3;
+    QPushButton *pushButton;
+    QLabel *label;
+    QPushButton *pushButton_2;
+    QLabel *label_2;
+    QLabel *label_3;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        first_text = new QTextEdit(centralwidget);
+        first_text->setObjectName(QString::fromUtf8("first_text"));
+        first_text->setGeometry(QRect(0, 0, 801, 231));
+        second_text = new QTextEdit(centralwidget);
+        second_text->setObjectName(QString::fromUtf8("second_text"));
+        second_text->setGeometry(QRect(0, 260, 801, 161));
+        textEdit_3 = new QTextEdit(centralwidget);
+        textEdit_3->setObjectName(QString::fromUtf8("textEdit_3"));
+        textEdit_3->setGeometry(QRect(0, 420, 801, 121));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(0, 230, 106, 30));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(230, 230, 151, 21));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(110, 230, 106, 30));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(400, 230, 181, 21));
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(560, 230, 80, 21));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 26));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Read", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "\320\222\320\226\320\243\320\245", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
